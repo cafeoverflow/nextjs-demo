e8a91c03728f7124e6f93b48b7c8e752867e8473
f3nghuajie <f3nghuajie@gmail.com>
2025-11-25 16:38:13 +0800

    error handling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,10 @@ import Hello from '../../components/hello'
 
 const Home = () => {
   console.log('test')
+
+  // the closest error boundary is in app/(root)/error.tsx
+  throw new Error('Test error for error boundary')
+
   return (
     <main>
       <div className='text-1xl'>Welcome to Next.js with TypeScript!</div>
